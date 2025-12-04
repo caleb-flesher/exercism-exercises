80a916212f62c5248c05843c78c09c25916e73b4
caleb-flesher <caleb.flesher@outlook.com>
2025-12-04 08:28:28 -0500

    Updated LogLevels to be cleaner.

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -1,24 +1,19 @@
+import static java.lang.String.format;
+
 public class LogLevels {
     
     public static String message(String logLine) {
-        String separator = ": ";
-        logLine.indexOf(separator);
-        return logLine.substring(logLine.indexOf(separator) + 2).trim();
+        return logLine.split("]: ")[1].trim();
     }
 
     public static String logLevel(String logLine) {
-        char separatorStart = '[';
-        char separatorEnd = ']';
-        logLine.indexOf(separatorStart);
-        logLine.indexOf(separatorEnd);
-        return logLine.substring(
-                logLine.indexOf(separatorStart) + 1, 
-                logLine.indexOf(separatorEnd))
+        return logLine.split("]: ")[0]
+            .substring(1)
             .trim()
             .toLowerCase();
     }
 
     public static String reformat(String logLine) {
-        return message(logLine) + " (" + logLevel(logLine) + ")";
+        return format("%s (%s)", message(logLine), logLevel(logLine));
     }
 }
