0cea0510d73a9edccdb55d17515cafcd8ca0fc58
caleb-flesher <caleb.flesher@outlook.com>
2025-12-04 09:04:58 -0500

    Annalyns Infiltration done.

---
+++ java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -0,0 +1,21 @@
+class AnnalynsInfiltration {
+    public static boolean canFastAttack(boolean knightIsAwake) {
+        return !knightIsAwake;
+    }
+
+    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
+    }
+
+    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+        return !archerIsAwake && prisonerIsAwake;
+    }
+
+    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
+        if ((knightIsAwake && !petDogIsPresent) || archerIsAwake) { return false; }
+        if (prisonerIsAwake || petDogIsPresent) {
+            return true;
+        }
+        return false;
+    }
+}
